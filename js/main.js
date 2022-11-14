b96bfedb5f9f4efa49bad27d07e5ec070478336c
Hayden Burnell <71723408+hburne11@users.noreply.github.com>
2022-11-14 13:59:29 -0500

    ~

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// import components first
+import HeroThumb from './components/TheHeroThumbnail.js';
+import LightBox from './components/TheLightbox.js';
+
+
+(() => {
+    // create vue instance here
+    const { createApp } = Vue
+
+    createApp({
+        created() {
+            // always get your remote data here
+            fetch('./data.json')
+                .then(res => res.json())
+                .then(data => this.heroData = data)
+            .catch(error => console.error(error));
+        },
+
+        data() {
+            return {
+                heroData: {},
+                lbData: {},
+                showLB: false
+
+            }
+        },
+
+        components: {
+            theherothumb: HeroThumb, //add theherothumb to the html and grab HeroThumb from the top of this js file
+            thelightbox: LightBox
+        },
+
+        methods: {
+            loadLightbox(hero){
+                //set the lightbox data / render it
+               this.lbData = hero;
+
+               //show the lightbox
+                this.showLB = true;
+
+            }
+
+        }
+    }).mount('#app')
+})()
