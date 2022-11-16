036c3cfc3764c3011036b3aee8513ee9d869aed5
Hayden Burnell <71723408+hburne11@users.noreply.github.com>
2022-11-16 12:53:56 -0500

    working lets goooo

--- js/components/TheLightbox.js
+++ js/components/TheLightbox.js
@@ -9,8 +9,10 @@ export default {
     <section class="lightbox">
         <img @click="closeLB" src="images/closeIcon.png" class="lightbox_close">
          <!-- hero image on the left, text on the right -->
+         <img src="images/PHEV.png" class="LBimg"></img>
         <!-- her image is a background image -->
         <article>
+      
             <p class="lb_text">
                 {{hero.desc}}The best of every world, combining fuel and electric power with ample cabin and cargo room.
             </p>
